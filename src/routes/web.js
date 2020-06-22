b011111e82daef2e642036cec04ae723de05b717
godbass <willofgod.wb@gmail.com>
2020-06-22 17:05:55 +0000

    deploy heroku

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,9 +1,13 @@
 import express from "express";
 import homepageController from "../controllers/homepageController";
+import chatbotController from "../controllers/chatbotController";
+
 let router = express.Router();
 
 let initWebRoutes = (app)=> {
     router.get("/", homepageController.getHomepage);
+    router.get("/webhook", chatbotController.getWebhook)
+    router.post("/webhook", chatbotController.postWebhook);
 
     return app.use("/", router);
 };
