579082fd4a6d0779f41504f46d9b5b01ee8ea299
godbass <willofgod.wb@gmail.com>
2020-06-23 18:57:54 +0000

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -138,7 +138,7 @@ function callSendAPI(sender_psid, response) {
     // Send the HTTP request to the Messenger Platform
     request({
         "uri": "https://graph.facebook.com/v6.0/me/messages",
-        "qs": { "access_token": process.env.FB_PAGE_TOKEN },
+        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
         "method": "POST",
         "json": request_body
     }, (err, res, body) => {
@@ -230,7 +230,7 @@ let callSendAPIWithTemplate = (sender_psid) => {
 
     request({
         "uri": "https://graph.facebook.com/v2.6/me/messages",
-        "qs": { "acces_token": process.env.FB_PAGE_TOKEN},
+        "qs": { "acces_token": process.env.PAGE_ACCESS_TOKEN},
         "method": "POST",
         "json": body
     },(err, res, body) => {
